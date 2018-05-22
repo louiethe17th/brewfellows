2597aa07f90fd5b1af0b27f8d2f653db42ca5c47
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-05-22 14:14:22 -0700

    fiddled things

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,44 +13,37 @@ var impIpa = 0;
 
 
 // var styleArray = [redAle, stout, porter, ipa, saison, pilsner, cider, impIpa];
-  var numberArray = [];
-  
+var numberArray = [];
 
-  
-  
-  //Check to see if beer list is already populated in local Storage
-  if (localStorage.localBeerList){
-      beerArray = JSON.parse(localStorage.getItem('localBeerList'));
-      console.log('pulled from LS');
-    } else {
-        console.log('Building list to store in LS');
-        populateList();
-        localStorage.setItem('localBeerList', JSON.stringify(beerArray));
-    };
-    
-    function getMaxOfArray(styleArray) {
-        return Math.max.apply(null, styleArray);
-    }
-    
-    
-    questionOne.addEventListener('click', testClick);
-    
-    function testClick(){
-        console.log('clickyBoi');
-        questionOne.removeEventListener('click', testClick)
-        sour ++;
-        
-    }
-    
-    
-    
-    var recommendBeer = function(){
+
+
+
+//Check to see if beer list is already populated in local Storage
+if (localStorage.localBeerList) {
+    beerArray = JSON.parse(localStorage.getItem('localBeerList'));
+    console.log('pulled from LS');
+} else {
+    console.log('Building list to store in LS');
+    populateList();
+    localStorage.setItem('localBeerList', JSON.stringify(beerArray));
+};
+
+function getMaxOfArray(styleArray) {
+    return Math.max.apply(null, styleArray);
+}
+
+
+
+
+
+
+var recommendBeer = function () {
     console.log(styleArray);
-    var styleArray = [ale, sour,  redAle, stout, porter, ipa, saison, pilsner, cider, impIpa];
-
-    for(i in styleArray){
-      numberArray.push(styleArray[i]);
-      console.log(numberArray);
+    var styleArray = [ale, sour, redAle, stout, porter, ipa, saison, pilsner, cider, impIpa];
+    
+    for (i in styleArray) {
+        numberArray.push(styleArray[i]);
+        console.log(numberArray);
     }
     
     var maxNumber = Math.max(...numberArray);
@@ -61,4 +54,7 @@ var impIpa = 0;
     numberArray = [];
     
     
-    }
+}
+
+
+
